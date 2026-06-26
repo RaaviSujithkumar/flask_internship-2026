4c4e689cf53f2a37f6c89e125a19c7e4344ef012
RaaviSujithkumar <raavisujith@gmail.com>
2026-06-26 15:57:51 +0530

    app.py

--- app.py
+++ app.py
@@ -1,112 +1,123 @@
-from flask import Flask, request,jsonify
+from flask import Flask, request, jsonify
 import psycopg2
+from flask_bcrypt import Bcrypt
 
-app =Flask(__name__)
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
-#database config
 DB_HOST = "localhost"
 DB_NAME = "postgres"
 DB_USER = "postgres"
 DB_PASSWORD = "2102"
 
+
 def get_db_connection():
     return psycopg2.connect(
-        host = DB_HOST,
-        database = DB_NAME,
-        user =DB_USER,
-        password =DB_PASSWORD
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
     )
 
-#CREATE STUDENT_TABLE
-def create_student_table():
+def create_users_table():
     connection = get_db_connection()
     cur = connection.cursor()
+
     cur.execute("""
-             CREATE TABLE IF NOT EXISTS student_table(
-                 student_id SERIAL PRIMARY KEY,
-                 student_name TEXT NOT NULL,
-                 roll_number TEXT NOT NULL UNIQUE,
-                 email TEXT NOT  NULL UNIQUE
-                );
-""")
+        CREATE TABLE IF NOT EXISTS users_table(
+            user_id SERIAL PRIMARY KEY,
+            username VARCHAR(100) UNIQUE NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            phone_number VARCHAR(15) NOT NULL,
+            college VARCHAR(100) NOT NULL
+        );
+    """)
     connection.commit()
     cur.close()
     connection.close()
-
-create_student_table()
-
-
-@app.route("/send_data", methods = ['POST'])
-def send_data():
-    student_name = request.json['student_name']
-    roll_number = request.json['roll_number']
-    email = request.json['email']
-    connection = get_db_connection()
-    cur = connection.cursor()
-    cur.execute("""
-        INSERT  INTO student_table(student_name ,roll_number, email) VALUES(%s,%s,%s)
-""",(student_name,roll_number,email))
-    connection.commit()
-    cur.close()
-    connection.close()
-    return jsonify({"message":"Data sended successfully"}),201
     
+create_users_table()
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+    phone_number = data.get("phone_number")
+    college = data.get("college")
 
-@app.route("/get_data", methods =['GET'])
-def get_data():
-    connection =get_db_connection()
-    cur =connection.cursor()
+    if not username or not email or not password or not phone_number or not college:
+        return jsonify({"error": "All fields are required"}), 400
+    connection = get_db_connection()
+    cur = connection.cursor()
     cur.execute("""
-          select * from student_table
-""")
-    Data = cur.fetchall()
-    cur.close()
-    connection.close()
-    results = []
-    for row in Data:
-        results.append({
-        "student_id":row[0],
-        "student-name":row[1],
-        "roll_number ":row[2],
-        "email":row[3]
-        })
-    return jsonify(results),200
-
-@app.route("/update_data",methods=['put'])
-def update_data():
-    student_id= request.json['student_id']
-    student_name = request.json['student_name']
-    roll_number = request.json['roll_number']
-    email = request.json['email']
-    connection=get_db_connection()
-    cur =connection.cursor()
+        SELECT * FROM users_table
+        WHERE username=%s OR email=%s
+    """, (username, email))
+    existing_user = cur.fetchone()
+    if existing_user:
+        cur.close()
+        connection.close()
+        return jsonify({
+            "error": "Username or Email already exists"
+        }), 400
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
     cur.execute("""
-          UPDATE student_table
-          SET student_name =%s,
-              roll_number=%s,
-              email=%s
-          WHERE student_id=%s 
-""",(student_name, roll_number, email, student_id))
+        INSERT INTO users_table
+        (username, email, password, phone_number, college)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (
+        username,
+        email,
+        hashed_password,
+        phone_number,
+        college
+    ))
     connection.commit()
     cur.close()
     connection.close()
-    return jsonify({"message":"Data updated successfully"}),200
+    return jsonify({
+        "message": "User Registered Successfully"
+    }), 201
 
-
-@app.route("/delete_data",methods=['DELETE'])
-def delete_data():
-    student_id= int(request.json['student_id'])
-    connection=get_db_connection()
-    cur =connection.cursor()
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+    if not username or not email or not password:
+        return jsonify({
+            "error": "All fields are required"
+        }), 400
+    connection = get_db_connection()
+    cur = connection.cursor()
     cur.execute("""
-          DELETE from student_table
-          WHERE student_id=%s 
-""",(student_id,))
-    connection.commit()
+        SELECT user_id, username, email, password
+        FROM users_table
+        WHERE username=%s AND email=%s
+    """, (username, email))
+    user = cur.fetchone()
     cur.close()
     connection.close()
-    return jsonify({"message":"Data deleted successfully"}),200
-
+    if not user:
+        return jsonify({
+            "error": "Username and Email do not match"
+        }), 401
+    user_id, username, email, hashed_password = user
+    if not bcrypt.check_password_hash(hashed_password, password):
+        return jsonify({
+            "error": "Invalid Password"
+        }), 401
+        return jsonify({
+        "message": "Login Successful",
+        "user": {
+            "user_id": user_id,
+            "username": username,
+            "email": email
+        }
+    }), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
